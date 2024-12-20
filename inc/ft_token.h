229615b98ee96dcc66a180c11d6dd8a2b12c75cb
Tetsuro Ando <teando@c2r6s10.42tokyo.jp>
2024-12-21 00:52:10 +0900

    norming and add include libft to token header

--- inc/ft_token.h
+++ inc/ft_token.h
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_token.h                                         :+:      :+:    :+:   */
@@ -6,56 +6,56 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 18:20:59 by teando            #+#    #+#             */
-/*   Updated: 2024/12/20 03:02:46 by teando           ###   ########.fr       */
+/*   Updated: 2024/12/21 00:50:28 by teando           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef TOKEN_H
-#define TOKEN_H
+# define TOKEN_H
+
+# include "libft.h"
 
 typedef enum e_token_type
 {
-	TT_CMD,			 // コマンドトークン
-	TT_PIPE,		 // パイプ
-	TT_REDIRECT_IN,	 // 入力リダイレクト
+	TT_CMD,          // コマンドトークン
+	TT_PIPE,         // パイプ
+	TT_REDIRECT_IN,  // 入力リダイレクト
 	TT_REDIRECT_OUT, // 出力リダイレクト
-	TT_APPEND,		 // 追記リダイレクト
-	TT_HEREDOC,		 // ヒアドキュメント
-	TT_LPAREN,		 // (
-	TT_RPAREN,		 // )
-	TT_AND_AND,		 // &&
-	TT_OR_OR,		 // ||
-	TT_SEMICOLON,	 // ;
-	TT_EOF,			 // 終端
-	TT_ERROR		 // エラー
-} t_token_type;
-
-
+	TT_APPEND,       // 追記リダイレクト
+	TT_HEREDOC,      // ヒアドキュメント
+	TT_LPAREN,       // (
+	TT_RPAREN,       // )
+	TT_AND_AND,      // &&
+	TT_OR_OR,        // ||
+	TT_SEMICOLON,    // ;
+	TT_EOF,          // 終端
+	TT_ERROR         // エラー
+}						t_token_type;
 
 //外に出さないとこれをnewする関数が作れない。。。
-//todo それぞれnew, free, aplly関数を作る
+// todo それぞれnew, free, aplly関数を作る
 typedef struct s_ast_node
 {
 	enum
 	{
-		NT_CMD,		 // words and redirects head ptr
-		NT_PIPE,	 // |
+		NT_CMD,      // words and redirects head ptr
+		NT_PIPE,     // |
 		NT_PRIORITY, // ()
-		NT_EOP,		 // && || ; EOF
+		NT_EOP,      // && || ; EOF
 	} e_type;
 	union
 	{
 		t_list *args; // CMDのitems WORDとリダイレクトの先頭ポインタ
-		int pipefds[2];
-		int priority_level;
-		t_token_type eop_type;
+		int				pipefds[2];
+		int				priority_level;
+		t_token_type	eop_type;
 	} u_val;
-} t_ast_node_item;
+}						t_ast_node_item;
 
 typedef struct s_token
 {
-	t_token_type type;
-	char *value;
-} t_token;
+	t_token_type		type;
+	char				*value;
+}						t_token;
 
 #endif
