66bf26724e47ad192c0243669bf51e483038ee0d
Tetsuro Ando <teando@c4r2s12.42tokyo.jp>
2024-12-23 05:20:06 +0900

    norming step4

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/15 18:05:31 by teando            #+#    #+#              #
-#    Updated: 2024/12/23 04:37:52 by teando           ###   ########.fr        #
+#    Updated: 2024/12/23 05:19:10 by teando           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -34,6 +34,8 @@ SRCS 		:= \
 		) \
 		$(addprefix 1_tokenizer/, \
 			allocate_token.c \
+			lexer_cmd_helpers.c \
+			lexer_loop.c \
 			lexer_utils.c \
 			lexer.c \
 			token_type_identifier.c \
--- inc/ft_lexer.h
+++ inc/ft_lexer.h
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 16:44:20 by teando            #+#    #+#             */
-/*   Updated: 2024/12/23 04:38:46 by teando           ###   ########.fr       */
+/*   Updated: 2024/12/23 05:18:13 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,28 @@ void			debug_print_token_list(t_list *list);
 
 /* <---------------- 内部用プロトタイプ ----------------> */
 
-// lexer.c
+// lexer_loop.c
 int				tokenize_line(t_info *info);
+
+// lexer.c
+char			*read_quoted(const char *line, size_t *pos, t_info *info);
+t_token			*parse_redirect(const char *line, size_t *pos, t_info *info);
+int				parse_one_command(const char *line, size_t *i, t_info *info);
+t_token			*get_operator_token(const char *line, size_t *pos,
+					t_info *info);
 
 // allocate_token.c
 t_token			*create_token(t_token_type type, char **value, t_info *info);
 int				add_token(t_info *info, t_token *tok);
+
+// lexer_cmd_helpers.c
+int				handle_redirect(const char *line, size_t *i, t_info *info,
+					t_list **redir_list);
+int				handle_quoted(const char *line, size_t *i, t_info *info,
+					char ***cmd_argv);
+int				handle_word(const char *line, size_t *i, t_info *info,
+					char ***cmd_argv);
+int				flush_redir_list(t_list **redir_list, t_info *info);
 
 // lexer_utils.c
 int				skip_spaces(const char *line, size_t *pos);
