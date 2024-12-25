7fdfd0778683e5ceebb7f0743686399ec9bc6435
QWERTOP18 <mz18yui@gmail.com>
2024-12-26 00:36:58 +0900

    .

--- src/2_parser/ast_root_new.c
+++ src/2_parser/ast_root_new.c
@@ -26,9 +26,10 @@ t_ast	*ast_node_new(int type, t_list *args, t_info *info)
 t_list	*consume_token(t_token_type type, t_list **lst)
 {
 	t_list	*args;
-
-	while ((*lst) != type)
+	t_token *token = (t_token *)(*lst)->data;
+	while (token->type != type)
 	{
+		
 	}
 }
 
@@ -37,6 +38,7 @@ t_ast	*ast_pipe_new(t_list *list, t_info *info)
 {
 	t_ast	*pipe_node;
 	t_ast	*current_node;
+	t_token *token = (t_token *)list->data;
 
 	pipe_node = astnew_node(NT_PIPE, NULL, info);
 	current_node = pipe_node;
@@ -50,3 +52,32 @@ t_ast	*ast_pipe_new(t_list *list, t_info *info)
 	}
 	return (pipe_node);
 }
+
+
+
+// t_ast	*astnew_node(int type, char *argv, t_info *info)
+// {
+// 	t_ast	*new_node;
+
+// 	new_node = xmalloc(sizeof(t_ast), info);
+// 	new_node->type = type;
+// 	new_node->args = ft_split(argv, ' ');
+// 	return (new_node);
+// }
+
+// t_ast	*ast_new(char **argv, t_info *info)
+// {
+// 	t_ast	*root_node;
+// 	t_ast	*current_node;
+
+// 	root_node = astnew_node(TOKEN_PIPE, NULL, info);
+// 	current_node = root_node;
+// 	while (*argv != NULL)
+// 	{
+// 		current_node->left = astnew_node(TOKEN_CMD, *argv++, info);
+// 		if (*argv)
+// 			current_node->right = astnew_node(TOKEN_PIPE, NULL, info);
+// 		current_node = current_node->right;
+// 	}
+// 	return (root_node);
+// }
