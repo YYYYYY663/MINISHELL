df64707d5c29a0ce0e1c71a5d5e27bb791536ad9
QWERTOP18 <mz18yui@gmail.com>
2024-12-27 04:05:46 +0900

    機能はしたけどsegfoとcloseのエラーが起こってる

--- src/3_executor/__3main.c
+++ src/3_executor/__3main.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv, char **env)
     t_info *info = system_init(env);
 
     // ft_putstrs_endl_fd(ft_list_to_strs(info->env_map), "\n",1);
-    info->source_line = ft_strdup("((ls -l) | cat && git add .) ; echo a");
+    info->source_line = ft_strdup("ls -l | cat | grep exec && ( echo second | cat )");
     launch_lexer(info);
     //debug_print_token_list(info->token_list);
     
@@ -32,3 +32,5 @@ int main(int argc, char **argv, char **env)
 
     return 0;
 }
+
+// "ls -l | cat | grep exec && echo second | cat"
--- src/3_executor/exec_traverse.c
+++ src/3_executor/exec_traverse.c
@@ -20,10 +20,89 @@
  */
 int g_signal = 0;
 
-
-t_status exec_pipe_node(t_ast *node, t_info *info)
+pid_t	cmd_node(t_ast *node, int in_fd, int out_fd, t_info *info)
 {
-	return E_NONE;
+	#ifdef FUNC_OUT
+		printf("%s node: %s\n",__func__, e_type_to_str(node->ntype));
+	#endif
+	pid_t	pid;
+	char	path[PATH_MAX];
+
+	//cmd, rd_i, rd_oの変数展開
+	//redirectの解決
+	char 	**argv = ft_list_to_strs(node->args->cmd);
+	ft_putstrs_endl_fd(argv,"\n",2);
+
+	if (in_fd == -1 || out_fd == -1)
+		return (-1);
+	
+	pid = xfork(info);
+	if (pid == 0)
+	{
+		#ifdef FUNC_OUT
+			dprintf(2,"in: %d\tout: %d\n",in_fd,out_fd);
+		#endif
+		if (in_fd != STDIN_FILENO)
+		{
+			dup2(in_fd, STDIN_FILENO);
+			xclose(&in_fd);
+		}
+		if (out_fd!=STDOUT_FILENO)
+		{
+			dup2(out_fd, STDOUT_FILENO);
+			xclose(&out_fd);
+		}
+		#ifdef FUNC_OUT
+			dprintf(2,"in: %d\tout: %d\n",in_fd,out_fd);
+		#endif
+		// if (fetch_absolutepath(path, *node->args, info->env_path, X_OK))
+		// 	process_exit(*node->args);
+		// execve(path, node->args, );
+		execvp(argv[0], argv);
+		exit(1);
+		// process_exit(*node->args);
+	}
+	return (pid);
+}
+
+t_status pipe_node(t_ast *node, int in_fd, int out_fd, t_info *info)
+{
+	#ifdef FUNC_OUT
+		printf("%s\n",__func__);
+	#endif
+	int pipefds[2];
+	int status = 0;
+	if (node->right != NULL)
+	{
+		xpipe(pipefds, info);
+		// if (node->left->ntype != NT_CMD)
+		// 	traverse_ast_nodes(node,info);
+		cmd_node(node->left, in_fd, pipefds[1], info);
+		xclose(&pipefds[1]);
+		pipe_node(node->right, pipefds[0], out_fd, info);
+		xclose(&pipefds[0]);
+	}
+	else
+	{
+		//todo builtinの確認
+		// if (node->left->ntype != NT_CMD)
+		// 	traverse_ast_nodes(node,info);
+		waitpid(cmd_node(node->left, in_fd, out_fd, info), &status, 0);
+		xclose(&in_fd);
+	}
+	return ((t_status)status);
+}
+
+
+t_status exec_pipeline(t_ast *node, t_info *info)
+{
+	#ifdef FUNC_OUT
+		printf("%s\n",__func__);
+	#endif
+	if (node->left->ntype == NT_CMD)
+		return pipe_node(node, STDIN_FILENO, STDOUT_FILENO, info);
+	else
+		(traverse_ast_nodes(node->left, info));
 }
 
 
@@ -41,7 +120,7 @@ t_status	traverse_ast_nodes(t_ast *node, t_info *info)
 	// if (node->ntype == NT_CMD)pipeの中で処理する
 
 	if (node->ntype == NT_PIPE)
-		return exec_pipe_node(node, info);
+		return exec_pipeline(node, info);
 	
 	if (node->ntype == NT_AND)
 	{
