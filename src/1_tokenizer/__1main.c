48c54b07dadef24e99973d8109a5c93863410ffd
QWERTOP18 <mz18yui@gmail.com>
2024-12-26 17:12:18 +0900

    tokenizerのMakefileを修正aファイルのみ作成されてそれをtestルールで実行する形にした

--- src/1_tokenizer/__1main.c
+++ src/1_tokenizer/__1main.c
@@ -4,5 +4,11 @@
 
 int main(int argc, char **argv, char **env) 
 {
+    t_info *info = system_init(env);
+
+    ft_putstrs_endl_fd(ft_list_to_strs(info->env_map), "\n",1);
+
+    system_deinit(info);
+
     return 0;
 }
