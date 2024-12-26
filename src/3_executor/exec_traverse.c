c2adba2a4de95b1ed7d68dff899570ce0c80005b
QWERTOP18 <mz18yui@gmail.com>
2024-12-27 04:17:13 +0900

    d1

--- src/3_executor/exec_traverse.c
+++ src/3_executor/exec_traverse.c
@@ -31,7 +31,9 @@ pid_t	cmd_node(t_ast *node, int in_fd, int out_fd, t_info *info)
 	//cmd, rd_i, rd_oの変数展開
 	//redirectの解決
 	char 	**argv = ft_list_to_strs(node->args->cmd);
-	ft_putstrs_endl_fd(argv,"\n",2);
+	#ifdef FUNC_OUT
+		ft_putstrs_endl_fd(argv,"\n",2);
+	#endif
 
 	if (in_fd == -1 || out_fd == -1)
 		return (-1);
