160ea68ebed74a369f4f89ec71f24636d0df84d5
QWERTOP18 <mz18yui@gmail.com>
2024-12-27 20:49:39 +0900

    debug option

--- src/3_executor/__3main.c
+++ src/3_executor/__3main.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv, char **env)
     t_info *info = system_init(env);
 
     // ft_putstrs_endl_fd(ft_list_to_strs(info->env_map), "\n",1);
-    info->source_line = ft_strdup("ls -l | grep Make && echo a | cat");
+    info->source_line = ft_strdup("ls -l | grep Make && echo a | cat > out ; <out cat");
     launch_lexer(info);
     //debug_print_token_list(info->token_list);
     
