a020e1d876be7519638497bce64c5e3691a9393b
Yui Mizukami <ymizukam@c2r8s1.42tokyo.jp>
2025-01-05 01:37:42 +0900

    exit

--- src/7_builtin/_builtin_exit.c
+++ src/7_builtin/_builtin_exit.c
@@ -6,15 +6,13 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 15:45:58 by teando            #+#    #+#             */
-/*   Updated: 2025/01/04 22:12:39 by ymizukam         ###   ########.fr       */
+/*   Updated: 2025/01/05 01:36:10 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_builtin.h"
 #include "ft_env.h"
 #include "ft_system.h"
-
-static int	_is_number(char *str);
 
 /*
 	1,引数が数字ではない
@@ -27,42 +25,29 @@ static int	_is_number(char *str);
 */
 t_status	__exit(const char *path, char **argv, t_info *info)
 {
-	int	exit_status;
+	long	exit_status;
+	char	*endptr;
 
 	exit_status = 0;
 	(void)path;
 	printf("exit\n");
 	if (argv[1] != NULL)
 	{
-		// todo xperror早めに作る
-		if (!_is_number(argv[1]))
+		exit_status = ft_strtol(argv[1], &endptr, 10);
+		if (endptr != argv[1] + ft_strlen(argv[1]) || exit_status > INT_MAX
+			|| exit_status < INT_MIN)
 		{
-			ft_putstr_fd("numeric argument required\n", STDERR_FILENO);
-			system_exit(info, 255);
+			ft_dprintf(2, "minishell: exit: %s: numeric argument required\n",
+				argv[1]);
+			system_exit(info, 2);
 		}
 		else if (argv[2])
 		{
-			ft_putstr_fd("too many arguments\n", STDERR_FILENO);
-			system_exit(info, 255);
+			ft_putstr_fd("minishell: exit: too many arguments\n",
+				STDERR_FILENO);
+			return (1);
 		}
-		exit_status = ft_atoi(argv[1]);
 	}
-	system_exit(info, exit_status);
-	return (1);
-}
-
-static int	_is_number(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str[0] == '-')
-		i++;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-			return (0);
-		i++;
-	}
+	system_exit(info, (t_status)exit_status % 0x100);
 	return (1);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 16:45:19 by teando            #+#    #+#             */
-/*   Updated: 2025/01/04 23:56:28 by ymizukam         ###   ########.fr       */
+/*   Updated: 2025/01/05 01:05:14 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,11 @@
 
 extern void	init_signals(void);
 
-// void	shell_loop(t_info *info)
-// {
-// 	while (1)
-// 	{
-// 		info->source_line = read_line_until_balanced(PROMPT);
-// 		if (!info->source_line)
-// 		{
-// 			ft_dprintf(STDOUT_FILENO, "exit\n");
-// 			break ;
-// 		}
-// 		if (xlexer(info) == E_NONE)
-// 		{
-// 			debug_print_token_list(info->token_list);
-// 			token_list_free(&info->token_list);
-// 		}
-// 		free(info->source_line);
-// 		info->source_line = NULL;
-// 	}
-// }
-
 void	shell_loop(t_info *info)
 {
 	while (1)
 	{
-		printf(BG_GREEN WHITE "%s", info->cwd);
-		printf(RESET " $ ");
-		fflush(stdout);
-		// ft_dprintf(1, BG_GREEN WHITE);
-		// info->source_line = read_line_until_balanced(info->cwd);
-		// ft_dprintf(1, RESET " $ ");
-		info->source_line = get_next_line(0);
+		info->source_line = read_line_until_balanced(PROMPT);
 		launch_lexer(info);
 		// debug_print_token_list(info->token_list);
 		launch_parser(info);
@@ -54,6 +28,26 @@ void	shell_loop(t_info *info)
 		line_init(info);
 	}
 }
+
+// void	shell_loop(t_info *info)
+// {
+// 	while (1)
+// 	{
+// 		printf(BG_GREEN WHITE "%s", info->cwd);
+// 		printf(RESET " $ ");
+// 		fflush(stdout);
+// 		// ft_dprintf(1, BG_GREEN WHITE);
+// 		// info->source_line = read_line_until_balanced(info->cwd);
+// 		// ft_dprintf(1, RESET " $ ");
+// 		info->source_line = get_next_line(0);
+// 		launch_lexer(info);
+// 		// debug_print_token_list(info->token_list);
+// 		launch_parser(info);
+// 		// debug_print_ast(info->ast, 0);
+// 		launch_executor(info);
+// 		line_init(info);
+// 	}
+// }
 
 int	main(int argc, char **argv, char **envp)
 {
