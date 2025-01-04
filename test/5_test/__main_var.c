810d60eaab94bb677dc84da0a95eb6230476bfa6
Yui Mizukami <ymizukam@c2r8s2.42tokyo.jp>
2025-01-05 02:36:13 +0900

    make var

--- test/5_test/__main_var.c
+++ test/5_test/__main_var.c
@@ -1,17 +1,19 @@
 
 
-
-#include "ft_system.h"
 #include "ft_env.h"
+#include "ft_system.h"
 
 int	main(int argc, char **argv, char **envp)
 {
-    (void)argc;
-    (void)argv;
 	t_info	*info;
-	info = system_init(envp);
 
-   
-    
+	(void)argc;
+	(void)argv;
+	info = system_init(envp);
+	info->source_line = ft_strdup("$NOEXIST $EXIST");
+	launch_lexer(info);
+	debug_print_token_list(info->token_list);
+	var_dispacher(info->token_list, info);
+	debug_print_token_list(info->token_list);
 	system_exit(info, 0);
 }
