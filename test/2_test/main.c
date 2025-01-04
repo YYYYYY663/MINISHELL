c8ef501d58e99c671748248962d3cd1697a2005f
Yui Mizukami <ymizukam@c2r8s1.42tokyo.jp>
2025-01-05 02:26:49 +0900

    env test

--- test/2_test/main.c
+++ test/2_test/main.c
@@ -8,20 +8,17 @@
 
 int	main(int argc, char **argv, char **env)
 {
+	t_info	*info;
+
 	(void)argc;
 	(void)argv;
-	t_info *info = system_init(env);
-
+	info = system_init(env);
 	// ft_putstrs_endl_fd(ft_list_to_strs(info->env_map), "\n",1);
-	info->source_line = ft_strdup("(((ls -l) && (cat)))");
+	info->source_line = ft_strdup("ls -l && cat");
 	launch_lexer(info);
 	// debug_print_token_list(info->token_list);
-
 	launch_parser(info);
-
 	// debug_print_ast(info->ast, 0);
-
 	system_exit(info, 0);
-
 	return (0);
 }
--- test/5_test/__main_env.c
+++ test/5_test/__main_env.c
@@ -6,7 +6,7 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 22:06:46 by ymizukam          #+#    #+#             */
-/*   Updated: 2025/01/04 22:06:47 by ymizukam         ###   ########.fr       */
+/*   Updated: 2025/01/05 02:25:20 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_info	*info;
+	char	*home;
 
 	(void)argc;
 	(void)argv;
 	info = system_init(envp);
-	// printf("PATH: %s\n",env_get("PATH",info));
-	// env_unset("HOME",info);
-	// printf("after unset: %s\n",env_get("HOME",info));
+	home = env_get("HOME", info);
+	printf("before unset: %s\n", home);
+	free(home);
+	env_unset("HOME", info);
+	home = env_get("HOME", info);
+	printf("after  unset: %s\n", home);
+	free(home);
 	// env_export("NEW_KEY=gokigennikaga!!",info);
 	// printf("export: %s\n",env_get("NEW_KEY",info));
 	// env_export("KEY=",info);
@@ -31,6 +36,7 @@ int	main(int argc, char **argv, char **envp)
 	// printf("NOEQUAL: %s\n",env_get("NOEQUAL",info));
 	// printf("NOTEXIST: %s\n",env_get("NOTEXIST",info));
 	env_export("KEY=a", info);
-	env_export("KEY =", info);
+	env_export("KEYERR =", info);
+	env_export("1KEYERR=", info);
 	system_exit(info, 0);
 }
