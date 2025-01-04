da8f3bd4420fe00f89ae468ced6db49cf9bf4655
Yui Mizukami <ymizukam@c2r7s1.42tokyo.jp>
2025-01-05 05:49:52 +0900

    resolve

--- src/2_parser/parser.c
+++ src/2_parser/parser.c
@@ -2,12 +2,9 @@
 
 t_status	launch_parser(t_info *info)
 {
-#ifdef FUNC_OUT
-	printf("%s\n", __func__);
-#endif
-	info->ast = expr(info->token_list, info);
-#ifdef FUNC_OUT
-	printf("%s done\n", __func__);
-#endif
+	t_list	*head;
+
+	head = info->token_list;
+	info->ast = expr(&head, info);
 	return (E_NONE);
 }
