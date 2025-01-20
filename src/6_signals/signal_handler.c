098e1318e84383445b52b673527fc04741877c0d
TetsuroAndo <tetsuro997@gmail.com>
2025-01-20 11:36:41 +0900

    add rl_event_hook

--- src/6_signals/signal_handler.c
+++ src/6_signals/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 01:35:45 by teando            #+#    #+#             */
-/*   Updated: 2025/01/17 22:30:59 by teando           ###   ########.fr       */
+/*   Updated: 2025/01/20 08:26:54 by teando           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -15,56 +15,48 @@
 #include <readline/readline.h>
 #include <stdio.h>
 
-/*
- * グローバル変数の定義
- */
 volatile sig_atomic_t g_signal_status = 0;
 
-/*
- * SIGINT (Ctrl+C) ハンドラ
- * シグナル受信を記録し、必要最小限の処理のみを行う
- */
+int rl_noob(void)
+{
+	return (0);
+}
+
 static void sigint_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
 		g_signal_status = SIGINT;
-		rl_on_new_line();
+		rl_done = 1;
 		rl_replace_line("", 0);
 		write(STDOUT_FILENO, "\n", 1);
-		rl_redisplay();
+		rl_on_new_line();
 	}
 }
 
-/*
- * SIGQUIT (Ctrl+\) ハンドラ
- * シグナル受信を記録するのみ
- */
 static void sigquit_handler(int signum)
 {
 	if (signum == SIGQUIT)
+	{
 		g_signal_status = SIGQUIT;
+	}
 }
 
-/*
- * シグナルハンドラの初期化
- */
 int init_signals(void)
 {
 	struct sigaction sa_int;
 	struct sigaction sa_quit;
 
-	/* SIGINT の設定 */
+	rl_event_hook = (rl_hook_func_t *)rl_noob;
 	sa_int.sa_handler = sigint_handler;
+	sa_int.sa_flags = SA_RESTART;
 	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_flags = 0;
-	if (!sigaction(SIGINT, &sa_int, NULL))
+	if (sigaction(SIGINT, &sa_int, NULL) == -1)
 		return (-1);
-	/* SIGQUIT の設定 */
 	sa_quit.sa_handler = sigquit_handler;
+	sa_quit.sa_flags = SA_RESTART;
 	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = 0;
-	if (!sigaction(SIGQUIT, &sa_quit, NULL))
+	if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
 		return (-1);
 	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 16:45:19 by teando            #+#    #+#             */
-/*   Updated: 2025/01/20 02:00:15 by teando           ###   ########.fr       */
+/*   Updated: 2025/01/20 08:20:17 by teando           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -19,16 +19,20 @@ static void shell_loop(t_info *info)
 {
 	while (1)
 	{
-		int status;
-
 		line_init(info);
+
 		ft_dprintf(2, BG_GREEN WHITE "%s" RESET "\n", info->cwd);
 		info->source_line = launch_readline(PROMPT);
 		if (info->source_line == NULL)
 			system_exit(info, info->status);
 		if (g_signal_status == SIGINT)
+		{
+			g_signal_status = 0;
+			free(info->source_line);
+			info->source_line = NULL;
 			continue;
-		status = launch_lexer(info) || launch_parser(info) || launch_executor(info);
+		}
+		int status = launch_lexer(info) || launch_parser(info) || launch_executor(info);
 		if (status != E_NONE)
 			system_exit(info, status);
 	}
@@ -40,7 +44,7 @@ int main(int argc, char **argv, char **envp)
 
 	(void)argc;
 	(void)argv;
-	if (!init_signals())
+	if (init_signals())
 		return (ft_dprintf(2, "signal setup failure\n"), 1);
 	info = system_init(envp);
 	// alias_update(RCFILE, info);
